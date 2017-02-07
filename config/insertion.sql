f954d02dac5bab88e3790095ce3226bb28af6079
Emeline Hourmand <shiro-x3@hotmail.fr>
2017-02-07 16:53:26 +0100

    Revert "remove insertion"
    This reverts commit 12cd7eaef1c1b6fdcab4b9f55a158b5bf4425bd5.

---
+++ config/insertion.sql
@@ -0,0 +1,9 @@
+USE TPGIT;
+
+INSERT INTO user VALUES (1,"Dupond","Jean","Dupjea","12345");
+INSERT INTO user VALUES (2,"Michou","Isabelle","Miisa","98745");
+
+INSERT INTO droits VALUES (1,NULL, "", "", "", 1);
+INSERT INTO droits VALUES (2,NULL, "", "", "", 1);
+INSERT INTO droits VALUES (3,NULL, "", "", "", 2);
+INSERT INTO droits VALUES (4,NULL, "", "", "", 2);
