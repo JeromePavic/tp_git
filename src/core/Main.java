2cdc26490a4910198912e6dd76911654c09696a3
Emeline Hourmand <shiro-x3@hotmail.fr>
2017-02-07 15:45:06 +0100

    remove user and droits into Main.java

--- src/core/Main.java
+++ src/core/Main.java
@@ -4,19 +4,6 @@ import entities.User;
 public class Main{
 	
 	public static void main(String[] args){
-		
-	User user1 = new User("Dupond","Jean","Dupjea","12345");
-	User user2 = new User("Michou","Isabelle","Miisa","98745");
-		
-	Droits droit1 = new Droits();
-	Droits droit2 = new Droits();
-	Droits droit3 = new Droits();
-	Droits droit4 = new Droits();
-	
-	user1.setDroits(droit1);
-	user1.setDroits(droit2);
-	user2.setDroits(droit3);
-	user2.setDroits(droit4);
 	
 	}
 }
